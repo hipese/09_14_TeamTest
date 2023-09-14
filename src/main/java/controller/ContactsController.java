8ac9f9b01235357b7461b473c211dc4c45956498
SJL <zun0352@naver.com>
2023-09-14 16:44:49 +0900

    Controller added

---
+++ src/main/java/controller/ContactsController.java
@@ -0,0 +1,61 @@
+package controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ContactsDAO;
+import dto.ContactsDTO;
+
+@WebServlet("*.contacts")
+public class ContactsController extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf8");
+
+		String cmd = request.getRequestURI();
+		System.out.println(cmd);
+
+		ContactsDAO dao = ContactsDAO.getInstance();
+		try {
+			if(cmd.equals("/Save.contacts")){
+
+				String writer = request.getParameter("writer");
+				String message = request.getParameter("message");
+				dao.insert(new ContactsDTO(0,writer,message));
+				response.sendRedirect("/index.html");
+			}
+			else if(cmd.equals("/List.contacts")){
+				java.util.List<ContactsDTO> list = dao.selectAll();
+				request.setAttribute("list", list);
+			
+				
+				request.getRequestDispatcher("/list.jsp").forward(request, response);
+			}
+			else if(cmd.equals("/Delete.contacts")){
+				String seq = request.getParameter("seq");
+				
+				dao.delete(seq);
+				response.sendRedirect("/List.contacts");
+			}
+			else if(cmd.equals("/Update.contacts")){
+				String writer = request.getParameter("writer");
+				String message = request.getParameter("message");
+				String seq = request.getParameter("seq");
+				
+				dao.update(writer,message,seq);
+				response.sendRedirect("/List.contacts");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			response.sendRedirect("/error.html");
+		}
+	}
+	
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
